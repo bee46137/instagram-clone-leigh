b3beb451025829937229efd745167f37de20ef16
Leigh Scheepers <86415012+bee46137@users.noreply.github.com>
2023-03-08 15:24:07 +0200

    added authentication and added functional logout button

---
+++ index.js
@@ -0,0 +1,88 @@
+class Application {
+
+    constructor(){
+
+        
+
+        // Initialize the FirebaseUI Widget using Firebase.
+        this.$application = document.querySelector("#application");
+        this.$firebaseAuthContainer = document.querySelector("#firebaseui-auth-container");
+        this.ui = new firebaseui.auth.AuthUI(firebase.auth());
+        this.$logoutButton = document.querySelector("#logoutButton");
+
+        this.addEventListeners();
+        this.handleAuth();
+
+    }
+
+        handleAuth (){
+            
+            firebase.auth().onAuthStateChanged((user) => {
+                if (user) {
+                  // User is signed in, see docs for a list of available properties
+                  // https://firebase.google.com/docs/reference/js/firebase.User
+                  var uid = user.uid;
+                  this.redirectToApp();
+                  // ...
+                } else {
+                  // User is signed out
+                  this.redirectToAuth();
+                }
+              });
+        }
+
+        redirectToAuth (){
+            this.$application.style.display = "none";
+            this.$firebaseAuthContainer.style.display = "block";
+
+            this.ui.start('#firebaseui-auth-container', {
+                callbacks: {
+                    signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+                      // User successfully signed in.
+                      // Return type determines whether we continue the redirect automatically
+                      // or whether we leave that to developer to handle.
+                      this.userId = authResult.user.uid;
+                      this.redirectToApp();
+                      this.$authUserText.innerHTML = user.displayName;
+                      // var uid = user.uid;
+                    },
+                  },
+                signInOptions: [
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                ],
+                // Other config options...
+            });
+        }
+
+        redirectToApp() {
+            this.$application.style.display = "block";
+            this.$firebaseAuthContainer.style.display = "none";
+        }
+
+        handleLogout () {
+            firebase.auth().signOut().then(() => {
+                alert("You've been logged out successfully.")
+                this.redirectToAuth();
+              }).catch((error) => {
+                alert("Apologies, an error occured. Error report sent.");
+                console.log(error);
+              });
+        }
+
+
+
+
+
+        addEventListeners(){
+
+            this.$logoutButton.addEventListener("click", (event) => {
+                this.handleLogout()
+            })
+    }
+
+
+
+}
+
+const application = new Application();
